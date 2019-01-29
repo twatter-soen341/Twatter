6e5d68fd38bfdd2b311c81f42c69471e7848b119
Paul <paul.musat@mail.concordia.ca>
2019-01-29 14:03:27 -0500

    revert unverified commit
    Accidentally pushed authentication to dev

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const cors = require('cors');
-const passport = require('passport');
 
 const indexRoutes = require('./routes/index');
-const authRoute = require('./routes/auth');
 
 var uri = process.env.DEV_DATABASE
 
@@ -17,7 +14,7 @@ if (process.env.NODE_ENV === "prod") {
     console.log("ATTENTION: connected to production database. Please ensure 500 mb data limit with MongoClient")
 }
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -29,16 +26,9 @@ db.once('open', () => {
 app.set('view-engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-/* Passport Middleware */
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./middleware/passport')(passport);
 
 //use routes
 app.use(indexRoutes)
-app.use('/api/auth', authRoute)
 
 app.listen(8080, () => {
     console.log("connected on port 8080")

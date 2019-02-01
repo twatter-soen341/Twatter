eba7fdda34e82cf928268820662379f24f803607
apaggabao <a.paggabao@hotmail.com>
2019-02-01 13:30:26 -0500

    Added route "/posts" to router module

--- twatter-ui/src/app/app-routing.module.ts
+++ twatter-ui/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import { PostListComponent } from './feed/posts/post-list/post-list.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'posts', component: PostListComponent }
 ];
 
 @NgModule({

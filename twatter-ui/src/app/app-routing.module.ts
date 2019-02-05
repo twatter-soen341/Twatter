cdeae89f9d55e4398f015917c1470b7a3a149d95
Dean <deancs1997@gmail.com>
2019-02-04 20:41:50 -0500

    Profile can now be accessed
    Added the link '/profile', which can now be accessed and started working on said page.

--- twatter-ui/src/app/app-routing.module.ts
+++ twatter-ui/src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import { PostListComponent } from './feed/posts/post-list/post-list.component';
+import {PostListComponent} from './feed/posts/post-list/post-list.component';
+import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'posts', component: PostListComponent }
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'posts', component: PostListComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- twatter-ui/src/app/profile/profile.component.ts
+++ twatter-ui/src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-profile',

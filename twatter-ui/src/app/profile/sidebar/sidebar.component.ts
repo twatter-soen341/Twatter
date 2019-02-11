da5fda7d6ab107ffd01ee01007de4449199a8f18
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2019-02-10 23:25:14 -0500

    #52 Removed scroll button from login/register

--- twatter-ui/src/app/profile/sidebar/sidebar.component.ts
+++ twatter-ui/src/app/profile/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  firstName = '';
+  lastName = '';
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.firstName = user.firstName;
+      this.lastName = user.lastName;
+    });
   }
 
 }

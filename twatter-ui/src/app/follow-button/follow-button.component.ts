2b9e4f4cc9e2e181187351b72155716697b1c745
narrapangan <narra_pangan@hotmail.com>
2019-03-05 17:07:27 -0500

    Refactored so that text of button will be updated after click.

--- twatter-ui/src/app/follow-button/follow-button.component.ts
+++ twatter-ui/src/app/follow-button/follow-button.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { ActivatedRoute} from '@angular/router';
+import { Router } from '@angular/router';
+import { User } from '../models/auth.model';
+import { stringify } from '@angular/core/src/util';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-follow-button',
@@ -8,42 +11,63 @@ import { ActivatedRoute} from '@angular/router';
   styleUrls: ['./follow-button.component.scss']
 })
 export class FollowButtonComponent implements OnInit {
+  private userToFollow;
+  private user: User;
+  private userId: string;
+  public btnText = 'Follow';
+  private followed: boolean;
 
-  @Input()
-  private otherUser;
-  private currentUser;
-  followTxt : string;
 
-  constructor(private userService:UserService, private route: ActivatedRoute) {}
+  constructor(private userService:UserService, private router: Router) {
+
+  }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      if(params['id'])
-        this.otherUser = params['id'];
-    });
+    // get user's id
+    this.userService.getCurrentUser()
+      .subscribe(user => {
+        this.user = user;
+        this.userId = user._id;
+        console.log(this.userId);
 
-    this.userService.getCurrentUser().subscribe(user => {
-      this.currentUser = user._id;
-      this.followTxt = user.following.includes(this.otherUser)? 'Unfollow': 'Follow';
+        // get id of user to follow
+        this.userToFollow = this.router.url.split('/profile/')[1];
+
+        this.alreadyFollowing();
     });
   }
 
-  isFollowing(array){
-    if(array){
-      return array.includes(this.otherUser);
-    }else{
-      return false;
-    }
+  // Check if initially followed and update button
+  alreadyFollowing() {
+    this.userService.getFollowers(this.userToFollow).subscribe(data => {
+      console.log(data.followers);
+      try {
+        for ( const user of data.followers ) {
+          if (user.id === this.user.id) {
+            this.btnText = 'Unfollow';
+            this.followed = true;
+          } else {
+            this.btnText = 'Follow';
+            this.followed = false;
+          }
+        }
+      } catch (error) {
+        console.log(data.message);
+        this.followed = false;
+      }
+    });
   }
 
+  // follow/unfollow and update button
   toggleFollow() {
-    if(this.followTxt == 'Unfollow'){
-      this.userService.unfollowUser(this.otherUser);
-      this.followTxt =  'Follow';
-    }else{
-      this.userService.followUser(this.otherUser);
-      this.followTxt = 'Unfollow';
+    if (this.followed) {
+      this.btnText = 'Follow';
+      this.followed = false;
+      this.userService.unfollowUser(this.userToFollow);
+    } else {
+      this.btnText = 'Unfollow';
+      this.followed = true;
+      this.userService.followUser(this.userToFollow);
     }
   }
-
 }

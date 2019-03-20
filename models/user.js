c6ea79c2d7022f9fe711aa1aac94f14b697fbadf
Paul <paul.musat@mail.concordia.ca>
2019-03-20 11:11:16 -0400

    #227 Added Bio to user model in backend

--- models/user.js
+++ models/user.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-
+  bio: String,
   following: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
 
   }],
-
   followers: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
